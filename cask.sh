c94d0bf74470bc5cd65c00621060b8f82fcd3c99
KallynGowdyYetiCGI <kallyn.gowdy@yeticgi.com>
2019-02-05 14:56:47 -0500

    Add cask and nvm scripts

---
+++ cask.sh
@@ -0,0 +1,24 @@
+
+# Script to install programs from brew cask
+
+brew tap caskroom/cask
+
+brew update
+
+# Normal tools
+brew cask install google-chrome
+brew cask install slack
+brew cask install 1password
+brew cask install teamviewer
+brew cask install the-unarchiver
+
+# Dev Tools
+brew cask install java
+brew cask install visual-studio-code
+brew cask install gitkraken
+brew cask install iterm2
+brew cask install unity-hub
+
+
+
+brew cleanup
