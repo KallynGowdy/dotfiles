bec986cd0d6392d0965316775327edb1ed09223d
Kallyn Gowdy <kallyngowdy@gmail.com>
2019-12-30 15:39:11 -0500

    Remove some unneeded cask steps

--- cask.sh
+++ cask.sh
@@ -20,11 +20,6 @@ brew cask install iterm2
 brew cask install unity-hub
 
 # Android SDK
-brew install ant
-brew install maven
-brew install gradle
-brew cask install android-sdk
-brew cask install android-ndk
 brew cask install android-studio
 
 # .Net
